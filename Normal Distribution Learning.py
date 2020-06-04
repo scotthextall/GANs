b3e15e5cefb96f7752a99f2e00ac4456055cceff
Scott Hextall <scotthextall@users.noreply.github.com>
2020-06-04 21:55:55 +0100

    Trying to fix sizes.

--- Normal Distribution Learning.py
+++ Normal Distribution Learning.py
@@ -10,26 +10,26 @@ import matplotlib.pyplot as plt
 mean = 4
 std_dev = 1.5
 
-#   Creating the normal distribution dataset.
+"""#   Creating the normal distribution dataset.
 dataset = np.random.normal(mean, std_dev, 1000)
 
 #   Visualising the dataset.
 plt.hist(np.random.normal(mean, std_dev, 1000), 50, density=True)
-plt.show()
+plt.show()"""
 
 
 #   Function to generate random normal distribution training data on the go. Also creates '1' labels to show it's real.
-def get_trainset(mu, sigma, n, batch_size):
+def get_trainset(mu, sigma, n):
     data = np.random.normal(mu, sigma, n)
     trainset = torch.tensor(data)
-    labels = [1] * batch_size
+    labels = [1] * n
     return labels, trainset
 
 
 #   Function to create inputs to pass into generator. Generator will output fake data.
 #   Linear inputs generated to make it more difficult for generator to trick discriminator.
-def get_generator_input(m, n):
-    generator_input = torch.rand(m, n)
+def get_generator_input(n):
+    generator_input = torch.rand(n)
     return generator_input
 
 
@@ -81,18 +81,21 @@ def train():
         g_optimiser.zero_grad()
 
         #   Creating random inputs for generator.
-        g_input = get_generator_input(batch_size, n).float()
+        g_input = get_generator_input(n).float()
         generated_data = G(g_input)
+        print(generated_data)
 
         #   Generate examples of real data.
-        real_labels, real_data = get_trainset(mean, std_dev, n, batch_size)
+        real_labels, real_data = get_trainset(mean, std_dev, n)
         real_labels = torch.tensor(real_labels).float()
         real_data = torch.tensor(real_data).float()
-        print(real_labels.size())
+        print(real_labels)
+        print(real_data)
 
-        """#   Training the generator. Check understanding of this bit.
+        #   Training the generator. Check understanding of this bit.
         generator_discriminator_out = D(generated_data)
         g_loss = loss(generator_discriminator_out, real_labels)
+        print(g_loss)
         g_loss.backward()
         g_optimiser.step()
 
@@ -106,7 +109,7 @@ def train():
         generator_discriminator_loss = loss(generator_discriminator_out, torch.zeros(batch_size))
         d_loss = (real_d_loss + generator_discriminator_loss) / 2
         d_loss.backward()
-        d_optimiser.step()"""
+        d_optimiser.step()
 
 train()
 
