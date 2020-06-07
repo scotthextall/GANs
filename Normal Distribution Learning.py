42bf102acf2af56adb89fc85b64c563bb38fff72
Scott Hextall <scotthextall@users.noreply.github.com>
2020-06-07 20:03:29 +0100

    Amends to improve GAN.

--- Normal Distribution Learning.py
+++ Normal Distribution Learning.py
@@ -6,11 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#   Mean and standard deviation of the normal distribution.
-mean = 4
-std_dev = 1.5
-
-
 #   Function to generate random normal distribution training data on the go. Also creates '1' labels to show it's real.
 def get_real_data(mu, sigma, n):
     np_array = np.random.normal(mu, sigma, n)
@@ -18,43 +13,11 @@ def get_real_data(mu, sigma, n):
     return dataset
 
 
-#print(get_real_data(mean, std_dev, 1000).size())
-
-
 #   Function to create noise of size n to pass into generator. Generator will output fake data of size n.
 #   Linear inputs generated to make it more difficult for generator to trick discriminator.
 def get_noise(n):
     noise = torch.rand(n)
     return noise
-
-
-#print(get_noise(1000).size())
-
-
-"""#   Building the Generator. Simple, single n neuron layer.
-#   Takes n 'noise' values and generates n fake data values.
-class Generator(nn.Module):
-
-    def __init__(self, input_size):
-        super(Generator, self).__init__()
-        self.dense_layer = nn.Linear(int(input_size), int(input_size))
-        self.activation = nn.Sigmoid()
-
-    def forward(self, x):
-        return self.activation(self.dense_layer(x))
-
-
-#   Building the Discriminator.
-#   Takes n values and outputs whether they're real or not.
-class Discriminator(nn.Module):
-
-    def __init__(self, input_size):
-        super(Discriminator, self).__init__()
-        self.dense = nn.Linear(int(input_size), 1)
-        self.activation = nn.Sigmoid()
-
-    def forward(self, x):
-        return self.activation(self.dense(x))"""
 
 
 class Generator(nn.Module):
@@ -88,43 +51,22 @@ class Discriminator(nn.Module):
         return self.f(self.map3(x))
 
 
-#array = np.random.normal(1, 2, 1000)
-#data = torch.from_numpy((array))
-#print(data.size())
-
-#noise = get_noise(1000)
-
-#plt.hist(noise.detach().numpy(), 100, density=True)
-
-#G = Generator(1000)
-#D = Discriminator(1000)
-#print(G)
-#print(D)
-
-#fake_data = G(noise)
-#print(fake_data)
-
-#plt.hist(fake_data.detach().numpy(), 100, density=True)
-#plt.show()
-
-#print(G(data.float()))
-
-
 #   Training the model.
 def train():
     #   Parameters
     mu = 4.5    # Mean of the real normal distribution data.
     sigma = 1.2     # Standard deviation of the real normal distribution data.
-    n = 1000     # Size of the training set and number of neurons in the single layer Generator nn.
-    lr = 1e-2   # Learning rate of the optimisers/models.
-    num_epochs = 100000   # Number of iterations to train models on.
-    g_input_size = 1
-    g_hidden_size = 50
-    g_output_size = 1
-    d_input_size = n
-    d_hidden_size = 100
-    d_output_size = 1
+    n = 1000    # Size of the training set and number of neurons in the single layer Generator nn.
+    lr = 1e-3   # Learning rate of the optimisers/models.
+    num_epochs = 100000     # Number of iterations to train models on.
+    g_input_size = 1    # Single node in Generator input layer -> Single value noise input.
+    g_hidden_size = 50  # Number of nodes in Generator hidden layers.
+    g_output_size = 1   # Single node in Generator output layer -> Single fake value output.
+    d_input_size = n    # n nodes in Discriminator input layer -> Takes n value input (Whole dataset).
+    d_hidden_size = 100 # Number of nodes in Discriminator hidden layers.
+    d_output_size = 1   # Single node in Discriminator output layer -> Single value 1 (real) or 0 (fake) output.
 
+    #   Activation functions for the Generator and Discriminators.
     g_activation_function = torch.tanh
     d_activation_function = torch.sigmoid
 
@@ -139,11 +81,8 @@ def train():
                       f=d_activation_function)
 
     #   Optimisers which update/improve models
-    """g_optimiser = torch.optim.Adam(G.parameters(), lr=lr)
-    d_optimiser = torch.optim.Adam(D.parameters(), lr=lr)"""
-
-    g_optimiser = torch.optim.SGD(G.parameters(), lr=lr, momentum=0.9)
-    d_optimiser = torch.optim.SGD(D.parameters(), lr=lr, momentum=0.9)
+    g_optimiser = torch.optim.Adam(G.parameters(), lr=lr)
+    d_optimiser = torch.optim.Adam(D.parameters(), lr=lr)
 
     #   Loss function (Binary Cross Entropy)
     loss = nn.BCELoss()
@@ -155,10 +94,6 @@ def train():
     #   Lists to store mu and sigma of the generated fake_data.
     mu_values = []
     sigma_values = []
-
-    noise = get_noise(n)
-    print(torch.reshape(noise, [n, 1]))
-    print(noise.size())
 
     for epoch in range(num_epochs):
         """1) Train Discriminator"""
@@ -225,13 +160,12 @@ def train():
         mu_values.append(mu_value)
         sigma_values.append(sigma_value)
 
-        #print(epoch)
-
         if epoch % 1000 == 0:
             print(epoch)
 
         """4) Plotting histogram of the generated fake_data to visualise how it changes as models are trained."""
         if epoch % 10000 == 0:
+            print("Mean: {}     Std_Dev: {}".format(fake_data_detached_np.mean(), fake_data_detached_np.std()))
             plt.hist(real_data.detach().numpy(), bins=100, density=True)
             plt.hist(fake_data_detached_np, bins=100, density=True)
             plt.show()
