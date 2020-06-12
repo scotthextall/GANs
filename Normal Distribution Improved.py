6a2dd95b7d03c7dd6295c0748fb49043fb27602e
Scott Hextall <scotthextall@users.noreply.github.com>
2020-06-12 19:12:12 +0100

    Removed time module.

--- Normal Distribution Improved.py
+++ Normal Distribution Improved.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 #   Function to generate random normal distribution training data on the go.
